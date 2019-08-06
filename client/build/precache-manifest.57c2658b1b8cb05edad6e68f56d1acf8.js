f931761c0a242a834e90773e033ed354985bf293
Ayan Banerjee <ayanb1999@gmail.com>
2019-08-06 15:44:16 +0530

    Build

---
+++ client/build/precache-manifest.57c2658b1b8cb05edad6e68f56d1acf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2d24ffcbd05d7b876482185d3cf29e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bef83337ebf574a64d0",
+    "url": "/static/css/main.96e30d52.chunk.css"
+  },
+  {
+    "revision": "b1db81d42b9d861c1fcd",
+    "url": "/static/js/2.7ead7da7.chunk.js"
+  },
+  {
+    "revision": "8bef83337ebf574a64d0",
+    "url": "/static/js/main.2811810c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9afa33f24212bf4364edf19dd7c134f0",
+    "url": "/static/media/Football homepage.9afa33f2.png"
+  },
+  {
+    "revision": "a99edb8cb78487ebd7a02fc15cdab217",
+    "url": "/static/media/logo.a99edb8c.svg"
+  }
+]);
